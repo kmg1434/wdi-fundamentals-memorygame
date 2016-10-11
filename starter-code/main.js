a31875054b6c9ed6be2838a5e11284b4f0b90ff7
Kevin Grady <Shrugs@KevBook-20.local>
2016-10-11 18:24:53 -0400

    added if statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,17 @@
 console.log("JS file is connected to HTML! Woo!")
+
+
+// four card variable initialization
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "king";
+
+
+if (cardTwo === cardFour){
+
+	alert('You found a match!');
+} 
+else {
+	alert('Sorry, try again.');
+}
