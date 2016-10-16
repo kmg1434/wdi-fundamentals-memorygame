fe66b51ce900a82027c70e8002d4680bbafe20f3
kmg1434 <kmg1434@gmail.com>
2016-10-16 18:04:00 -0400

    added createCards method

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,11 +7,26 @@ var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "king";
 
+var board = document.getElementById('game-board');
 
+var createCards = function(cardCount){
+
+	cardCount = 4;
+
+	for (var i=1; i<=cardCount; i++){
+
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		board.appendChild(newCard);
+	}
+}
+
+/*
 if (cardTwo === cardFour){
 
 	alert('You found a match!');
-} 
+}
 else {
 	alert('Sorry, try again.');
 }
+*/
